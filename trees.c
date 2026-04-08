3e035894ee21bbadc8d7ae918ecaefcc7e62635c
sashayudenko <youdenkosasha2008@gmail.com>
2026-04-09 00:15:27 +0200

    Mój wzór na dzewki

---
+++ trees.c
@@ -0,0 +1,367 @@
+//
+// Created by Sanechek on 02/04/2026.
+//
+
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct Node {
+    struct Node *left;
+    struct Node *right;
+    int key;
+} Node;
+
+Node* create_node(int value);
+Node* insert_bst(Node* root, int value);
+Node* insert_avl(int left, int right, int array[]);
+Node* remove_node(Node* root, int key);
+Node* deleting(Node* root);
+Node* rotate_right(Node* root);
+Node* rotate_left(Node* root);
+void print_postorder(Node* root);
+void print_preorder(Node* root);
+void print_inorder(Node* root);
+int find_min(Node *root);
+int find_max(Node* root);
+void dsw_rebalance(Node** root_ptr);
+void make_compression(Node* dumny, int count);
+
+void quick_sort(int array[], int left, int right);
+void print_tikz(Node* root);
+
+int main(int argc, char const *argv[]) {
+    Node *root = NULL;
+    int w_tree;
+    int n;
+    srand(time(NULL));
+
+    for (int i = 1; i <argc; i++) {
+        if (strcmp(argv[i],"--tree") == 0) {
+            if (i+1 < argc && strcmp( argv[i+1],"AVL")== 0) {
+                w_tree = 1;
+            }
+            else if (i+1 < argc && strcmp(argv[i+1],"BST")== 0) {
+                w_tree = 2;
+            }
+        }
+    }
+
+    printf("nodes> ");
+    fflush(stdout);
+    scanf("%d", &n);
+
+    printf("insert> ");
+    int *arr = malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if (w_tree == 2) {
+        for (int i = 0; i < n; i++) {
+            root = insert_bst(root, arr[i]);
+        }
+    } else if (w_tree == 1) {
+        quick_sort(arr, 0, n - 1);
+        root = insert_avl(0, n - 1, arr);
+    }
+
+    char action[20];
+    while (1) {
+        printf("action> ");
+        fflush(stdout);
+
+        if (scanf("%s", action) == EOF) {
+            break;
+        }
+        if (strcmp(action, "Export") == 0) {
+            printf("\n--- LaTeX TikZ Code ---\n");
+            printf("\\documentclass{article}\n");
+            printf("\\usepackage[utf8]{inputenc}\n");
+            printf("\\usepackage{tikz}\n");
+            printf("\\begin{document}\n"); // Не забудь про начало документа!
+            printf("\\begin{tikzpicture}[nodes={draw, circle}, level distance=1.5cm, \n");
+            printf("  level 1/.style={sibling distance=3cm}, \n");
+            printf("  level 2/.style={sibling distance=1.5cm}]\n");
+            printf("\\"); // Это начало команды \node, которую напечатает print_tikz
+
+            print_tikz(root);
+
+            printf(";\n");
+            printf("\\end{tikzpicture}\n");
+            printf("\\end{document}\n");
+        } else if (strcmp(action, "Remove") == 0) {
+            int value;
+            printf("Remove> ");
+            fflush(stdout);
+            while (scanf("%d", &value) == 1) {
+                root = remove_node(root, value);
+            }
+            // while (getchar() != '\n');
+        } else if (strcmp(action, "Exit")==0) {
+            break;
+        } else if (strcmp(action, "Print") == 0) {
+            printf("In-order: ");
+            print_inorder(root);
+            printf("\n");
+            printf("Post-order: ");
+            print_postorder(root);
+            printf("\n");
+            printf("Pre-order: ");
+            print_preorder(root);
+            printf("\n");
+        } else if (strcmp(action, "Find_min_max") == 0) {
+            int min, max;
+            min = find_min(root);
+            max = find_max(root);
+            printf("Max: %d\n", max);
+            printf("Min: %d\n", min);
+        } else if (strcmp(action, "Delete") == 0) {
+            printf("Deleting: ");
+            root = deleting(root);
+            printf("\n");
+        } else if (strcmp(action, "Rebalance")==0) {
+            dsw_rebalance(&root);
+            print_preorder(root);
+        }
+
+
+
+    }
+
+    free(arr);
+}
+
+Node* create_node(int value) {
+    Node *new_node = malloc(sizeof(Node));
+    new_node->key = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+Node* insert_bst(Node* root, int value) {
+    if (root == NULL) {
+        root = create_node(value);
+    } else if (value < root->key) {
+        root->left = insert_bst(root->left, value);
+    } else if (value > root->key) {
+        root->right = insert_bst(root->right, value);
+    }
+    return root;
+}
+
+Node* insert_avl(int left, int right, int array[]) {
+    if (left > right) {
+        return NULL;
+    }
+
+    int mid = (left + right) / 2;
+
+    Node* root = create_node(array[mid]);
+
+    root->left = insert_avl(left, mid -1, array);
+    root->right = insert_avl(mid + 1, right, array);
+
+    return root;
+}
+
+void quick_sort(int array[], int left, int right) {
+    int random = rand() % (right - left + 1) + left;
+
+    if (left < right) {
+        int temp = array[left];
+        array[left] = array[random];
+        array[random] = temp;
+
+        int pivot = array[left];
+        int i = left;
+
+        for (int j = left +1; j <= right; j++) {
+            if (array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp_last = array[left];
+        array[left] = array[i];
+        array[i] = temp_last;
+
+        quick_sort(array, left, i - 1);
+        quick_sort(array, i + 1, right);
+    }
+}
+
+Node* remove_node(Node* root, int key) {
+    if (root == NULL) {
+        return root;
+    }
+
+    if (key < root->key) {
+        root->left = remove_node(root->left, key);
+    } else if (key > root->key) {
+        root->right = remove_node(root->right, key);
+    }
+    else {
+        if (root->left == NULL) {
+        Node* temp = root->right;
+        free(root);
+        return temp;
+        }
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        Node* temp = root->right;
+        while (temp->left != NULL) {
+            temp = temp->left;
+        }
+        root->key = temp->key;
+        root->right = remove_node(root->right, temp->key);
+    }
+    return root;
+}
+
+void print_inorder(Node* root) {
+    if (root == NULL) {return;}
+    print_inorder(root->left);
+    printf("%d ", root->key);
+    print_inorder(root->right);
+}
+
+void print_preorder(Node* root) {
+    if (root == NULL) {return;}
+    printf("%d ", root->key);
+    print_preorder(root->left);
+    print_preorder(root->right);
+}
+
+void print_postorder(Node* root) {
+    if (root == NULL) {return;}
+    print_postorder(root->left);
+    print_postorder(root->right);
+    printf("%d ", root->key);
+
+}
+
+int find_max(Node* root) {
+    if (root == NULL) {return 2;}
+    Node* p = root;
+    while (p->right != NULL) {
+        p = p->right;
+    }
+    return p->key;
+}
+
+int find_min(Node *root) {
+    if (root == NULL) return 2;
+    Node* p = root;
+    while (p->left != NULL) {
+        p = p->left;
+    }
+    return p->key;
+}
+
+Node* deleting(Node* root) {
+    if (root == NULL) {return NULL;}
+    root->left = deleting(root->left);
+    root->right = deleting(root->right);
+    printf("%d ", root->key);
+    free(root);
+    return NULL;
+}
+
+Node* rotate_right(Node* root) {
+    if (root == NULL || root->left == NULL) {return root;}
+    Node* child = root->left;
+    root->left = child->right;
+    child->right = root;
+    return child;
+}
+
+Node* rotate_left(Node* root) {
+    if (root == NULL || root->right == NULL) {return root;}
+    Node* child = root->right;
+    root->right = child->left;
+    child->left = root;
+    return child;
+}
+
+void dsw_rebalance(Node** root_ptr) {
+    if (*root_ptr == NULL) {return;}
+
+    Node dummy;
+    dummy.right = *root_ptr;
+    dummy.left = NULL;
+
+    Node* p = &dummy;
+    while (p->right != NULL) {
+        Node* curr = p->right;
+        if (curr->left != NULL) {
+            p->right = rotate_right(curr);
+        } else {
+            p = p->right;
+        }
+    }
+
+    int lenth = 0;
+    Node* temp = dummy.right;
+    while (temp != NULL) {
+        lenth++;
+        temp = temp->right;
+    }
+
+    int h = (int)log2(lenth+1);
+    int m = (int) pow (2, h) -1;
+
+    make_compression(&dummy, lenth-m);
+
+    while (m > 1) {
+        m = m / 2;
+        make_compression(&dummy, m);
+    }
+    *root_ptr = dummy.right;
+}
+
+void make_compression(Node* dumny, int count) {
+    Node* p = dumny;
+    for (int i = 0; i < count; i++) {
+        if (p->right == NULL) {break;}
+        Node* child = p->right;
+        p->right = rotate_left(child);
+        p = p->right;
+    }
+}
+
+void print_tikz(Node* root) {
+
+    if (root == NULL) return;
+
+    printf("node {%d}\n", root->key);
+
+    if (root->left != NULL || root->right != NULL) {
+        if (root->left != NULL) {
+            printf("child { ");
+            print_tikz(root->left);
+            printf("} ");
+        } else {
+            printf("child[missing] ");
+        }
+
+        if (root->right != NULL) {
+            printf("child { ");
+            print_tikz(root->right);
+            printf("} ");
+        } else {
+            printf("child[missing] ");
+        }
+    }
+}
